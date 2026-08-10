f136a39640d3b93a6c2bc7df6c4a2bfb277031ac
Sebantonio <sebantonio@gmail.com>
2026-08-10 19:47:38 +0200

    fix: actualizar copy-exe.js para ruta correcta y nombre ESO_Varias_Activiades

--- scripts/copy-exe.js
+++ scripts/copy-exe.js
@@ -4,13 +4,13 @@ const path = require('path');
 const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const version = pkg.version;
 
-const bundleDir = 'C:\\cargo-target\\plantillaNotas\\release\\bundle\\nsis';
+const bundleDir = path.join(__dirname, '..', 'src-tauri', 'target', 'release', 'bundle', 'nsis');
 const exeDir = path.join(__dirname, '..', 'exe');
 
 fs.mkdirSync(exeDir, { recursive: true });
 
-const src = path.join(bundleDir, `ESO_notas_Actividades_${version}_x64-setup.exe`);
-const dst = path.join(exeDir, `ESO_notas_Actividades_${version}_x64-setup.exe`);
+const src = path.join(bundleDir, `ESO_Varias_Activiades_${version}_x64-setup.exe`);
+const dst = path.join(exeDir, `ESO_Varias_Activiades_${version}_x64-setup.exe`);
 
 fs.copyFileSync(src, dst);
-console.log(`Copiado: exe/ESO_notas_Actividades_${version}_x64-setup.exe`);
+console.log(`Copiado: exe/ESO_Varias_Activiades_${version}_x64-setup.exe`);
